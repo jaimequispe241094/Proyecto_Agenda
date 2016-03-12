81da754fda5c10afeee69b4beea236279e5f3739
jaime241094 <jaime241094@a2d3d8a1-c5c7-4531-b59c-d500ada57762>
2016-03-12 21:37:33 +0000

    ABM de localidad y TipoContacto realizado(sin persistencia)

--- Labo-UNGS-tag1/Agenda/Agenda/src/presentacion/controlador/ControladorTipoContacto.java
+++ Labo-UNGS-tag1/Agenda/Agenda/src/presentacion/controlador/ControladorTipoContacto.java
@@ -8,6 +8,8 @@ public class ControladorTipoContacto implements ActionListener
 {
 
 	private TipoContactoPanel tipoContacto;
+	private int indiceModificar ;
+	
 	
 	public ControladorTipoContacto(TipoContactoPanel panel)
 	{
@@ -15,11 +17,15 @@ public class ControladorTipoContacto implements ActionListener
 		this.tipoContacto.getBtnAgregar().addActionListener(this);
 		this.tipoContacto.getBtnModificar().addActionListener(this);
 		this.tipoContacto.getBtnBorrar().addActionListener(this);
+		this.tipoContacto.getBtnGuardar().addActionListener(this);
+		this.tipoContacto.getBtnCancelar().addActionListener(this);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) 
-	{
+	{	
+		
+		
 		if(e.getSource() == this.tipoContacto.getBtnAgregar())
 		{
 			if(this.tipoContacto.getTxtTipoContacto().getText().length() > 0)
@@ -41,10 +47,38 @@ public class ControladorTipoContacto implements ActionListener
 		{
 			if(this.tipoContacto.getListaTipoContactos().getSelectedIndex() != -1)
 			{
+				this.mostrarUnGrupoDeBotones(false, true);
 				this.tipoContacto.getTxtTipoContacto().setText(tipoContacto.getListaTipoContactos().getSelectedItem());
+				indiceModificar = this.tipoContacto.getListaTipoContactos().getSelectedIndex();
 			}
 		}
 		
+		else if(e.getSource() == this.tipoContacto.getBtnCancelar())
+		{
+			this.mostrarUnGrupoDeBotones(true, false);
+			tipoContacto.getTxtTipoContacto().setText("");
+		}
+		
+		else if(e.getSource() == this.tipoContacto.getBtnGuardar())
+		{
+			String tipo = this.tipoContacto.getTxtTipoContacto().getText();
+			if(tipo.length() > 0 && !tipo.equals(tipoContacto.getListaTipoContactos().getItem(indiceModificar)))
+			{
+				this.tipoContacto.getListaTipoContactos().remove(indiceModificar);
+				this.tipoContacto.getListaTipoContactos().add(tipo,indiceModificar);
+			}
+			this.mostrarUnGrupoDeBotones(true, false);
+			tipoContacto.getTxtTipoContacto().setText("");
+		}
+	}
+	
+	public void mostrarUnGrupoDeBotones(boolean verABM,boolean verEdicion)
+	{
+		tipoContacto.getBtnAgregar().setVisible(verABM);
+		tipoContacto.getBtnBorrar().setVisible(verABM);
+		tipoContacto.getBtnModificar().setVisible(verABM);
+		tipoContacto.getBtnGuardar().setVisible(verEdicion);
+		tipoContacto.getBtnCancelar().setVisible(verEdicion);	
 	}
 
 }
