26979155c7a55b6376e1aa20fd02b605c154a3cb
Jonatan_Diaz <Jonatan_Diaz@a2d3d8a1-c5c7-4531-b59c-d500ada57762>
2016-03-15 15:56:17 +0000

    query mysql agregadas

--- Labo-UNGS-tag1/Agenda/Agenda/sql/scriptAgenda.sql
+++ Labo-UNGS-tag1/Agenda/Agenda/sql/scriptAgenda.sql
@@ -1,9 +1,35 @@
 CREATE DATABASE `agenda`;
 USE agenda;
+CREATE TABLE `localidad` 
+(
+  `idLocalidad` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`idLocalidad`)
+);
+
+USE agenda;
+CREATE TABLE `tipoContacto` 
+(
+  `idTipo` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`idTipo`)
+);
+
+USE agenda;
 CREATE TABLE `personas` 
 (
   `idPersona` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(45) NOT NULL,
   `Telefono` varchar(20) NOT NULL,
-  PRIMARY KEY (`idPersona`)
+  `Calle` varchar(45) NOT NULL,
+  `Altura` int(11) NOT NULL,
+  `Piso` int(11) NOT NULL,
+  `Depto` int(11) NOT NULL,
+  `Localidad` int(11) NOT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Cumpleaños` varchar(45) NOT NULL,
+  `TipoDeContacto` int(11) NOT NULL,  
+  PRIMARY KEY (`idPersona`),
+  FOREIGN KEY (`Localidad`) REFERENCES localidad (`idLocalidad`),
+  FOREIGN KEY (`TipoDeContacto`) REFERENCES tipoContacto (`idTipo`)
 );
